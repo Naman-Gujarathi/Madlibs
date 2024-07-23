c588e70d03292dcb4b35705933e2c8f6e1d3a9fb
Naman-Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-07-23 19:04:23 -0400

    git coe updated

--- thankyou_email.py
+++ thankyou_email.py
@@ -14,3 +14,10 @@ Yours sincerely,
 Recruiting Team"""
 
 print(madlib)
+
+
+candidate_name1 = input("enter candidate_name1:" )
+
+madlib1 = f"Dear {candidate_name1} \n We want to thank you for your interest in the position of Software Developer and for all the time you have put into the application process.\n Unfortunately, you were not among those selected for further consideration. \n On behalf of the company, we wish you the best in your future career endeavors, please keep checking the positions on our career pages and follow us on social media.\n We will retain your information on file according to our Recruitment Privacy Policy >> https://www.volarisgroup.com/recruitment-privacy.\n Yours sincerely,\n Recruiting Team"
+
+print(madlib1)
