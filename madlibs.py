ade478e65f5e7fd6581a6f2d91ed20d446d10427
Naman-Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-07-23 18:57:42 -0400

    thankyou email template update

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+# youtuber = "Naman Gujarathi"
+
+# print("subscribe to " + youtuber)
+
+# print("subcribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective:" )
+verb1 = input("verb1:" )
+verb2 = input("verb2:" )
+famous_person = input("famous_person:" )
+
+madlib = f"Computer programming is so {adj} ! it makes me so excited all the time naman \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
---
+++ thankyou_email.py
@@ -0,0 +1,16 @@
+candidate_name = input("enter candidate_name:" )
+
+madlib = f"""Dear {candidate_name}
+
+We want to thank you for your interest in the position of Software Developer and for all the time you have put into the application process. 
+
+Unfortunately, you were not among those selected for further consideration. 
+
+On behalf of the company, we wish you the best in your future career endeavors, please keep checking the positions on our career pages and follow us on social media. 
+
+We will retain your information on file according to our Recruitment Privacy Policy >> https://www.volarisgroup.com/recruitment-privacy.
+
+Yours sincerely,
+Recruiting Team"""
+
+print(madlib)
